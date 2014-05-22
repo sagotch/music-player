fa5480206e96d683ba26d009075e71c5d6cea1e9
Julien Sagot <ju.sagot@gmail.com>
2014-05-22 18:05:17 +0200

    Used data-ng- instead of ng-.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" ng-app>
+<html lang="en" data-ng-app>
   <head>
     <meta charset="utf-8">
     <script type="text/javascript">
@@ -15,18 +15,18 @@
     <script type="text/javascript" src="./public/app/js/controllers.js"></script>
     <script type="text/javascript" src="./public/app/js/filters.js"></script>
   </head>
-  <body ng-controller="DirectoriesList">
+  <body data-ng-controller="DirectoriesList">
     <div class="span4" id="albums">
       <h3>Artist / Albums</h3>
       <table class="table table-bordered">
-        <tr ng-repeat="dir in directories">
+        <tr data-ng-repeat="dir in directories">
           <td>
-            <h4 ng-click="selectAlbum(dir.name)">
+            <h4 data-ng-click="selectAlbum(dir.name)">
               {{dir.name}}
             </h4> 
             <div class="album link"
-                 ng-repeat="album in dir.children"
-                 ng-click="selectAlbum(dir.name+'/'+album.name)">
+                 data-ng-repeat="album in dir.children"
+                 data-ng-click="selectAlbum(dir.name+'/'+album.name)">
               {{album.name}}
             </div>
           </td>
@@ -38,22 +38,22 @@
         Songs
       </h3>
       <div class="btn disabled"
-           ng-show="!directorysongs.length"
-           ng-click="addAllSongs(directorysongs)">
+           data-ng-show="!directorysongs.length"
+           data-ng-click="addAllSongs(directorysongs)">
         Add All
       </div>
       <div class="btn"
-           ng-show="directorysongs.length"
-           ng-click="addAllSongs(directorysongs)">
+           data-ng-show="directorysongs.length"
+           data-ng-click="addAllSongs(directorysongs)">
         Add All
       </div>
-      <p ng-show="!directorysongs.length">
+      <p data-ng-show="!directorysongs.length">
         <strong>&larr; Choose an album first.</strong>
       </p>
-      <table class="table table-bordered" ng-show="directorysongs.length">
-        <tr ng-repeat="song in directorysongs">
+      <table class="table table-bordered" data-ng-show="directorysongs.length">
+        <tr data-ng-repeat="song in directorysongs">
           <td>
-            <span ng-click="addSong(song)" class="link">
+            <span data-ng-click="addSong(song)" class="link">
               {{song.name}}
             </span>
           </td>
@@ -66,30 +66,31 @@
         <audio id="player"></audio>
       </div>
       <div class="btn disabled"
-           ng-show="!playlistsongs.length"
-           ng-click="deleteAllSongs()">
+           data-ng-show="!playlistsongs.length"
+           data-ng-click="deleteAllSongs()">
         Clear playlist
       </div>
       <div class="btn"
-           ng-show="playlistsongs.length"
-           ng-click="deleteAllSongs()">
+           data-ng-show="playlistsongs.length"
+           data-ng-click="deleteAllSongs()">
         Clear playlist
       </div>
-      <p ng-show="!playlistsongs.length">
+      <p data-ng-show="!playlistsongs.length">
         <strong>&larr; Select a song to add it to playlist.</strong>
       </p>
-      <table class="table table-bordered" ng-show="playlistsongs.length">
-        <tr ng-repeat="song in playlistsongs">
+      <table class="table table-bordered" data-ng-show="playlistsongs.length">
+        <tr data-ng-repeat="song in playlistsongs">
           <td>
             <span class="song link current"
-                  ng-show="$index==currentSongIndex">
+                  data-ng-show="$index==currentSongIndex">
               {{song.name}}
             </span>
             <span class="song link"
-                  ng-show="$index!=currentSongIndex"
-                  ng-click="playSong($index)">
+                  data-ng-show="$index!=currentSongIndex"
+                  data-ng-click="playSong($index)">
               {{song.name}}
-              <span ng-click="deleteSong($index)" class="delete icon-trash">
+              <span data-ng-click="deleteSong($index)"
+                    class="delete icon-trash">
               </span>
               </>
           </td>
