a047d496f78794f4481e1a75236ab17e5cd216ae
Julien Sagot <ju.sagot@gmail.com>
2014-05-23 12:14:18 +0200

    Formatting + added a comment about type + removed useless variable.

--- public/app/directories/directories.php
+++ public/app/directories/directories.php
@@ -1,4 +1,7 @@
 <?php
+
 require_once "../../config.php";
+
 require_once "./scan_directories.php";
+
 ?>
--- public/app/directories/songs.php
+++ public/app/directories/songs.php
@@ -1,34 +1,47 @@
 <?php
-require_once "../../config.php";
-$return = array();
-$get_dir = isset($_GET['dir'])?strip_tags($_GET['dir']):false;
 
-function error_message(){
-    echo json_encode(array("data" => array(), "error" => true));
-    die();
+require_once "../../config.php";
+
+$get_dir = isset ($_GET['dir']) ? strip_tags ($_GET['dir']) : false;
+
+function error_message ()
+{
+    echo json_encode (array ("data" => array (), "error" => true));
+    die ();
 }
 
-if($get_dir===false){
-    error_message();
+if ($get_dir === false)
+{
+    error_message ();
 }
 
 $request_dir = $root.$get_dir;
-if(realpath($request_dir)===false){
-    error_message();
+
+if (realpath ($request_dir) === false)
+{
+    error_message ();
 }
-if(strstr($request_dir, $root) === false){
-    error_message();
+if (strstr ($request_dir, $root) === false)
+{
+    error_message ();
     // tries to list no music dirs
 }
 
-if(!is_readable($request_dir)){
-    error_message();
+if (! is_readable ($request_dir))
+{
+    error_message ();
 }
-$return = array();
-$list = scandir($request_dir);
-foreach($list as $file){
-    if(!is_dir($request_dir.$file) && strstr($file, ".mp3")){
-        $return[] = array(
+
+$return = array ();
+
+$list = scandir ($request_dir);
+
+foreach ($list as $file)
+{
+    if (! is_dir($request_dir.$file)
+        && strstr($file, ".mp3"))
+    {
+        $return [] = array (
             "image" => "./public/img/default.png", 
             "name" => $file,
             "path" => $get_dir . "/" . $file
@@ -36,5 +49,6 @@ foreach($list as $file){
     }
 }
 
-echo json_encode($return);
+echo json_encode ($return);
+
 ?>
