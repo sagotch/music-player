023287298a8a76dc9445e6676abc901ad45efa55
Julien Sagot <ju.sagot@gmail.com>
2014-05-24 14:04:35 +0200

    Cleaner use of angularjs: used conditions to add class when needed. Clicking the current song in the playlist now restart it.

--- index.php
+++ index.php
@@ -48,37 +48,34 @@
         <div id="musicplayer">
           <audio id="player"></audio>
         </div>
-        <div class="btn disabled"
-             data-ng-show="!playlistsongs.length"
+        <div class="btn"
+             data-ng-class="{'disabled': !playlistsongs.length}"
              data-ng-click="deleteAllSongs()">
           Clear playlist
         </div>
         <div class="btn"
-             data-ng-show="playlistsongs.length"
-             data-ng-click="deleteAllSongs()">
-          Clear playlist
-        </div>
-        <div class="btn" data-ng-click="prevSong()">
+             data-ng-class="{'disabled': !playlistsongs.length}"
+             data-ng-click="prevSong()">
           <<
         </div>
-        <div class="btn" data-ng-click="nextSong()">
+        <div class="btn"
+             data-ng-class="{'disabled': !playlistsongs.length}"
+             data-ng-click="nextSong()">
           >>
         </div>
-        <p class="do-first" data-ng-show="!playlistsongs.length">
+        <p class="do-first" data-ng-if="!playlistsongs.length">
           <strong>Select a song.</strong>
         </p>
-        <table class="table table-bordered" data-ng-show="playlistsongs.length">
+        <table class="table table-bordered"
+               data-ng-if="playlistsongs.length">
           <tr data-ng-repeat="song in playlistsongs track by $index">
             <td>
-              <span class="song link current"
-                    data-ng-show="$index==currentSongIndex">
-                {{song.name}}
-              </span>
               <span class="song link"
-                    data-ng-show="$index!=currentSongIndex"
+                    data-ng-class="{'current': $index==currentSongIndex}"
                     data-ng-click="playSong($index)">
                 {{song.name}}
-                <span data-ng-click="deleteSong($index)"
+                <span data-ng-if="$index!=currentSongIndex"
+                      data-ng-click="deleteSong($index)"
                       class="delete icon-trash">
                 </span>
               </span>
@@ -93,19 +90,15 @@
         </h3>
         <img src="{{directorycover}}" alt="{{directorycover}}" />
         <div class="btn disabled"
-             data-ng-show="!directorysongs.length"
+             data-ng-class="{'disabled': !directorysongs.length}"
              data-ng-click="addAllSongs(directorysongs)">
           Add All
         </div>
-        <div class="btn"
-             data-ng-show="directorysongs.length"
-             data-ng-click="addAllSongs(directorysongs)">
-          Add All
-        </div>
-        <p class="do-first" data-ng-show="!directorysongs.length">
+        <p class="do-first" data-ng-if="!directorysongs.length">
           <strong>Choose an album.</strong>
         </p>
-        <table class="table table-bordered" data-ng-show="directorysongs.length">
+        <table class="table table-bordered"
+               data-ng-if="directorysongs.length">
           <tr data-ng-repeat="song in directorysongs">
             <td>
               <span data-ng-click="addSong(song)" class="link">
