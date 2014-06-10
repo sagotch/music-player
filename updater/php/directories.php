31c75757ee9e04879e78a284fd12e0eafea5a135
Julien Sagot <ju.sagot@gmail.com>
2014-06-10 21:07:43 +0200

    Use updater.php for JSON file generation. Based on existing application functions.

--- updater/php/directories.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once "../../config.php";
-
-require_once "./scan_directories.php";
-
-?>
---
+++ updater/php/updater.php
@@ -0,0 +1,103 @@
+<?php
+
+function scan_dir ($dir)
+{
+    return array_diff (scandir ($dir), array('.', '..') ) ;
+}
+
+/**
+ * @return php representation of album object
+ * {
+ *   "cover": "cover_file.jpg",
+ *   "songs": ["track-01.ogg", "track-02.ogg, ..."]
+ * }
+ */
+function album ($dir, $name)
+{
+
+    $album = array () ;
+
+    $album ['title'] = $name ;
+
+    $album ['cover'] = "" ;
+
+    $album ['songs'] = array () ;
+
+    $dirEntries = scan_dir ($dir) ;
+
+    foreach ($dirEntries as $entrie)
+    {
+        if (stristr ($entrie, '.mp3')
+            || stristr ($entrie, '.ogg')
+            || stristr ($entrie, '.m4a'))
+        {
+            $album ['songs'] [] = $entrie ;
+        }
+        else if (stristr ($entrie, '.jpg')
+                 || stristr ($entrie, '.png'))
+        {
+            $album ['cover'] = $entrie ;
+        }
+    }
+
+    return $album ;
+}
+
+/**
+ * @return 
+ * {
+ *   "name" : "artist name",
+ *   "albums" : [ album list ]
+ * }
+ */
+function artist ($dir, $name)
+{
+
+    $artist = array () ;
+
+    $artist ['name'] = $name;
+
+    $artist ['albums'] = array () ;
+
+    $dirEntries = scan_dir ($dir) ;
+
+    foreach ($dirEntries as $entrie)
+    {
+        if (is_dir ("$dir/$entrie"))
+        {
+            $artist ['albums'] [] = album ("$dir/$entrie", $entrie) ;
+        }
+    }
+
+    return $artist;
+}
+
+function library ($dir)
+{
+
+    $artists = array () ;
+
+    $dirEntries = scan_dir ($dir) ;
+
+    foreach ($dirEntries as $entrie)
+    {
+        if (is_dir ("$dir/$entrie"))
+        {
+            $artists [] = artist ("$dir/$entrie", $entrie) ;
+        }
+    }
+
+    return $artists;
+}
+
+/*** main ***/
+
+$library = array () ;
+
+$library ['prefix'] = $argv [1] ;
+
+$library ['artists'] = library ($argv [2]) ;
+
+echo json_encode ($library) . "\n" ;
+
+?>
